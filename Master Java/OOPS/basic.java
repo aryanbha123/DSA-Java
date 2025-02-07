8eb979b20ce5cce5eafac9749d5746c6987982f1
Aryanbha123 <Aryanbhandari4077@gmail.com>
2025-02-07 08:17:28 +0530

    Updates

--- Master Java/OOPS/basic.java
+++
@@ -1,7 +0,0 @@
-package OOPS;
-
-public class basic {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java");
-    }
-}
---
+++ Stack/Stack.java
@@ -0,0 +1,15 @@
+package Stack;
+
+public class Stack {
+    int []arr;
+    int n,top;
+
+    void push(int val){
+        if(top<n)
+            arr[n++]=val;
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
